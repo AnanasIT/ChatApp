418b784cc03f7938bf9152b31cdb0fd359eb41e5
Demid Efremov <demidefremov8@gmail.com>
2026-08-01 18:31:19 +0300

    Добавил helper для хэша паролей

---
+++ PasswordHelper/PasswordHelper.cs
@@ -0,0 +1,20 @@
+namespace PasswordHelperModel;
+
+using System.Security.Cryptography;
+using System.Text;
+
+public class HashPassword
+{
+    public string PasswordHash(string password)
+    {
+        using var sha256 = SHA256.Create();
+        var bytes = Encoding.UTF8.GetBytes(password);
+        var hash = sha256.ComputeHash(bytes);
+        return Convert.ToBase64String(hash);
+    }
+
+    public bool VerifyPassword(string password, string hashPassword)
+    {
+        return hashPassword == PasswordHash(password);
+    }
+}
