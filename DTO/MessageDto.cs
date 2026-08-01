4529d58faf2cec21f879e363b422b3725f3a012b
Demid Efremov <demidefremov8@gmail.com>
2026-08-01 18:01:08 +0300

    Добавил DTO для регистрации, аутентификации, сообщения и авторизации

---
+++ DTO/MessageDto.cs
@@ -0,0 +1,9 @@
+namespace MessageDto;
+
+public class MessageDto
+{
+    public int Id {get; set;}
+    public string UserName {get; set;} = string.Empty;
+    public string Content {get; set;} = string.Empty;
+    public DateTime SentAt {get; set;} = DateTime.UtcNow;
+}
