f51a9d475c0bafa87fe3ae02fe1367453274c7f2
Demid Efremov <demidefremov8@gmail.com>
2026-08-12 14:49:20 +0300

    обновил модели данных

--- ChatServer/Models/Message.cs
+++ ChatServer/Models/Message.cs
@@ -9,6 +9,11 @@ public class Message
     public string Content {get; set;} = string.Empty;
     public DateTime SentAt {get; set;} = DateTime.UtcNow;
 
+    public bool IsEdited {get; set;} = false;
+    public bool IsDeleted {get; set;} = false;
+    public DateTime? EditedAt {get; set;}
+    public DateTime DeletedAt {get; set;}
+
     public int UserId {get; set;}
     public User User {get; set;} = null!;
 
