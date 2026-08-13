7e1c61a89f846e8726edf55533d1d28edcb765c1
Demid Efremov <demidefremov8@gmail.com>
2026-08-13 14:43:32 +0300

    добавил дто для личного сообщения

---
+++ ChatServer/DTO/DirectMessageDto.cs
@@ -0,0 +1,13 @@
+namespace DirectMessageDTO;
+
+public class DirectMessageDto
+{
+    public int Id {get; set;}
+    public int SenderId {get; set;}
+    public string SenderName {get; set;} = string.Empty;
+    public int ReceiverId {get; set;}
+    public string ReceiverName {get; set;} = string.Empty;
+    public string Content {get; set;} = string.Empty;
+    public DateTime SentAt {get; set;}
+    public bool IsRead {get; set;}
+}
