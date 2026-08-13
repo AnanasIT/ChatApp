4d4434ba869c3de2a4a797210223323afaf84ae4
Demid Efremov <demidefremov8@gmail.com>
2026-08-13 14:43:00 +0300

    добавил дто для личного чата

---
+++ ChatServer/DTO/DirectChatRoomDto.cs
@@ -0,0 +1,11 @@
+namespace DirectChatRoomDTO;
+
+public class DirectChatRoomDTO
+{
+    public int Id {get; set;}
+    public int OtherUserId {get; set;}
+    public string OtherUserName {get; set;} = string.Empty;
+    public string LastMessage {get; set;} = string.Empty;
+    public DateTime LastMessageAt  {get; set;}
+    public int UnreadCount {get; set;}
+}
