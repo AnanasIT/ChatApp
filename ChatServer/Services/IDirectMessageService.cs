7931d8afeba9fe2d806cc904942a09d56b4b3607
Demid Efremov <demidefremov8@gmail.com>
2026-08-15 14:02:55 +0300

    добавил интерфейс сервиса для работы с личными и сообщениями и комнатами

---
+++ ChatServer/Services/IDirectMessageService.cs
@@ -0,0 +1,17 @@
+using ServiceResultModel;
+using DirectChatRoomModel;
+using DirectMessageModel;
+
+using DirectMessageDTO;
+using DirectChatRoomDTO;
+
+namespace IDirectMessageServiceModel;
+
+public interface IDirectMesasgeService
+{
+    Task<ServiceResult<List<DirectChatRoomDto>>> GetAllChatRoomsAsync(int userId);
+    Task<ServiceResult<List<DirectMessageDto>>> GetMessagesAsync(int userId, int otherUserId, int limit = 50);
+    Task<ServiceResult<DirectMessageDto>> SendMessageAsync(int senderId, int receiverId, string content);
+    Task<ServiceResult<bool>> MarkAsReadAsync(int userId, int otherUserId);
+    Task<ServiceResult<bool>> DeleteMessageAsync(int messageId, int userId);
+}
